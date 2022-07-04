80176ff52b821599ddbae0251fec9600974971ba
louzi233 <lyh_726@163.com>
2022-07-05 00:23:27 +0200

    update setup

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ setup(
     name='AssessmentFramework',
     version='0.1',
     license='MIT',
-    packages=find_packages{'realisticAF'},
+    packages=find_packages('realisticAF'),
     package_dir={'': 'realisticAF'},
+    url='https://github.com/louzi233/AssessmentFramework',
     install_requires=[
         'opencv-python',
         'numpy',
-        'pillow',
-    ]
+        'Pillow',
+      ],
+
 )
